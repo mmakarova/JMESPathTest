269abb44ec90edaf22df93644198c35dfa9fa539
mmakarova <makarova.marina.andreevna@gmail.com>
2024-04-07 19:57:42 +0300

    firstTest

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = {
+
+    "pack": [{ "paybackTypeCode": "PaybackType_AFT" }, { "paybackTypeCode": "PaybackType_AFT1" }]
+    ,
+    "content": [
+        {
+            "op": "add",
+            "path": "/terminals1/-",
+            "value": {
+                "guid": "70b1b9fc-8407-4efd-9b85-480eeb9c9be2",
+                "tradeSpotGuid": "5fd1da9d-e879-4f18-bd6c-e94d95bfc4a8",
+                "paybackTypeCode": "PaybackType_A2C",
+                "paybackDealGuid": "4f555a9d-e879-4f18-bd6c-e94d933fc477"
+            }
+        },
+        {
+            "op": "add",
+            "path": "/terminals2/-",
+            "value": {
+                "guid": "1111111c-8407-1efd-9b85-480eeb9c9be2",
+                "tradeSpotGuid": "5fd1da9d-e879-4f18-bd6c-e94d95bfc4a8",
+                "paybackTypeCode": "PaybackType_AFT",
+                "paybackDealGuid": "4f555a9d-e879-4f18-bd6c-e94d933fc477"
+            }
+        },
+        {
+            "op": "add",
+            "path": "/terminals1/-",
+            "value": {
+                "guid": "70b1b9fc-8407-4efd-9b85-480eeb9c9be2",
+                "tradeSpotGuid": "5fd1da9d-e879-4f18-bd6c-e94d95bfc4a8",
+                "paybackTypeCode": "PaybackType_A2C",
+                "paybackDealGuid": "4f555a9d-e879-4f18-bd6c-e94d933fc477"
+            }
+        },
+    ]
+
+}
+
+const dataArray = {
+    "EmptyList": [],
+    "NotEmptyList": [{ "paybackTypeCode": "PaybackType_AFT" }, { "paybackTypeCode": "PaybackType_AFT1" }]
+}
